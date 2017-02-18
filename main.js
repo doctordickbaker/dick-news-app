644ef8c22432075db906fb2095bb2ae53cf2ca43
doctordickbaker <doctordickbaker@gmail.com>
2017-02-18 12:26:32 -0600

    Node stuff happening

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  ///win.webContents.openDevTools()
+  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -57,3 +57,5 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
